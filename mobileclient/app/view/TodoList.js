8faf8cf424a161f5e8440547b87adddb6a296097
kbln28 <kbln28@gmx.de>
2014-07-10 16:21:47 +0200

    mobileclient erst mal rausgenommen
    mobileclient erst mal rausgenommen: Ladezeit verkürzt sich so erheblich

--- mobileclient/app/view/TodoList.js
+++
@@ -1,9 +0,0 @@
-Ext.define('Todoliste.view.TodoList', {
-	extend: 'Ext.dataview.List',			// Einbindung der Klasse dataview.List
-	xtype: 'todolist',						// Todoliste schreiben
-		config: {
-			store: 'Todos',					// Einbindung des Stores (aus /app/store/Todos.js)
-			itemTpl: '<div>{title}</div>',	// Template für den Titel
-			emptyText: 'keine Todos'		// Falls die Liste keine Einträge enthält
-		}
-});
